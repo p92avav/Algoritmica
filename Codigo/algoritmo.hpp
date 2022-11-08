b15ad5321259d1c408a63d1688c3daab2e092a66
Valentín <114603272+p92avav@users.noreply.github.com>
2022-11-08 12:10:55 +0100

    P4 Started

---
+++ Codigo/algoritmo.hpp
@@ -0,0 +1,14 @@
+#ifndef ALGORITMO_HPP
+#define ALGORITMO_HPP
+
+#include <iostream>
+
+using namespace std;
+
+void metodo1();
+
+void metodo2();
+
+segmentacionMinISE(serieTemporal serie, int segPoints);
+
+#endif
